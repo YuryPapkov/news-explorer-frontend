6e4ddd10f246cb12d7b251673a355f94910ca947
Yury Papkov <yu-papkoff@yandex.ru>
2021-01-26 15:42:42 +0300

    fixed: card marked as li instead of div

--- src/components/NewsCardList/newsCardList.js
+++ src/components/NewsCardList/newsCardList.js
@@ -31,15 +31,6 @@ function NewsCardList({ isLoggedIn, isTypeSavedCards }) {
     </div>
 
 
-    // <footer className="footer" >
-    //   <p className="footer__copyright">© 2020 Supersite, Powered by News API</p>
-    //   <div className="footer__link-container">
-    //     <Link to="/" className="footer__link">Главная</Link>
-    //     <a href="https://praktikum.yandex.ru" className="footer__link">Яндекс.Практикум</a>
-    //     <a href="https://github.com" className="footer__icon"><img src={githubPath} alt="логотип Гитхаба" /></a>
-    //     <a href="https://fb.com" className="footer__icon"><img src={fbPath} alt="логотип Фейсбук" /></a>
-    //   </div>
-    // </footer>
   );
 }
 export default NewsCardList;
